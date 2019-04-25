f0ff483d5416f4ffdbd74cc6146deccbaf8ab939
Your Name <you@example.com>
2019-04-24 23:37:59 -0700

    add HW3_Collatz.py

---
+++ HW3_Collatz.py
@@ -0,0 +1,35 @@
+def collatz(num):
+    if num %2 == 0:
+        even = num // 2
+        print (even)
+        return even
+            
+    elif num %2 == 1:
+        odd = 3*num +1
+        print(odd)
+        return odd
+
+
+
+print('num:')
+
+while True:
+    try:
+        num_input = int(input())
+        if num_input == 1:
+            print('Enter an integer >1')
+        elif num_input>1:
+            num_input =collatz(num_input)
+            while True:
+                if num_input == 1:
+                    break
+                else:
+                    num_input=collatz(num_input)
+        else:
+            print('Enter positive integer to begin')
+                      
+    except:
+        print('Not valid Integer')
+        break
+
+
